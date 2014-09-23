6a421692fbd71404d841521ff8829a38d9254984
Fabien O'Carroll <fabien@allou.is>
2014-09-23 21:00:48 +0100

    Add basic points api

---
+++ server/routers/points.js
@@ -0,0 +1,54 @@
+var express = require('express');
+
+var Points = require('../models/Points');
+
+var points = new express.Router();
+
+points.get('/', function (req, res, next) {
+  Points.find({}, function (err, results) {
+    console.log('Points.find:: err:', err, 'results:', results);
+    if (err) {
+      return next(err);
+    }
+    res.json(results);
+    res.end();
+  });
+});
+
+points.post('/', function (req, res, next) {
+  var body = req.body;
+  
+  if (!body.name) {
+    res.send(401, 'Need a name field');
+  }
+
+  if (!body.coordinates) {
+    res.send(401, 'Need a coordinates array');
+  }
+
+  var point = Points.create({
+    name: body.name,
+    loc: {
+      type: 'Point',
+      coordinates: body.coordinates
+    }
+  });
+
+  res.json(point.toObject());
+  res.end();
+  
+});
+
+points.get('/:name', function (req, res, next) {
+  Points.findOne({name: req.params.name}, function (err, result) {
+    if (err) {
+      return next(err);
+    }
+    res.json(result);
+  });
+});
+
+exports.handler = points;
+exports.path = '/api/points';
+
+
